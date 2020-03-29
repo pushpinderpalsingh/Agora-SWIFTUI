1822bef4c7aaf7ddb73c728be57a88714fe53aa4
Pushpinder Pal Singh <pushpinder@tutamail.com>
2020-03-29 21:45:03 +0530

    Search and ProfileImage View

---
+++ Agora/NavigationView.swift
@@ -0,0 +1,22 @@
+//
+//  NavigationView.swift
+//  Agora
+//
+//  Created by Pushpinder Pal Singh on 29/03/20.
+//  Copyright © 2020 Pushpinder Pal Singh. All rights reserved.
+//
+
+import SwiftUI
+
+struct NavigationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NavigationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView()
+            .previewLayout(.fixed(width: 375, height: 115))
+    }
+}
