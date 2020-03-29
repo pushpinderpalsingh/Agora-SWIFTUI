46f37cb4ed19b6ee6e623bd8d206463d9086d77d
Pushpinder Pal Singh <pushpinder@tutamail.com>
2020-03-29 22:20:11 +0530

    NavigationView

--- Agora/NavigationView.swift
+++ Agora/NavigationView.swift
@@ -10,13 +10,40 @@ import SwiftUI
 
 struct NavigationView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            VStack {
+                Spacer()
+                Spacer()
+                ZStack{
+                    Circle()
+                        .foregroundColor(.white)
+                        .frame(width: 45, height: 45)
+                    
+                    Image(systemName: "plus")
+                        .resizable()
+                        .frame(width: 25, height: 25)
+                    
+                }
+                HStack{
+                    Spacer()
+                    Image(systemName: "house")
+                        .resizable()
+                        .frame(width: 30, height: 28)
+                    Spacer()
+                    Spacer()
+                    Image(systemName: "person")
+                        .resizable()
+                        .frame(width: 30, height: 28)
+                    Spacer()
+                }
+                Spacer()
+                Spacer()
+            }.background(Color.gray)
     }
 }
 
 struct NavigationView_Previews: PreviewProvider {
     static var previews: some View {
         NavigationView()
-            .previewLayout(.fixed(width: 375, height: 115))
+            .previewLayout(.fixed(width: 375, height:  100))
     }
 }
